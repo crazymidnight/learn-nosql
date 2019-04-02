6d755390a0562b5e11374764a4c55f24a075400a
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-04-02 20:16:08 +0700

    add ISO date formatting

--- mongodb/add_records.py
+++ mongodb/add_records.py
@@ -1,12 +1,20 @@
 from pymongo import MongoClient
 import json
+import datetime
 
 
 with open("../sample.json") as f:
     users = [x for x in json.load(f)]
 
+for user in users:
+    user["date_of_birth"] = datetime.datetime.strptime(
+        user["date_of_birth"], "%d/%m/%Y"
+    )
+
 client = MongoClient()
 
 db = client.phonebook
+
+db.records.remove()
 
 db.records.insert_many(users)
