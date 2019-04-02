1ed3b0675b0a36e74d472b7d5b298c2374954e8d
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-04-02 20:16:30 +0700

    add simple pipeline

--- mongodb/aggregation.py
+++ mongodb/aggregation.py
@@ -5,6 +5,15 @@ client = MongoClient()
 
 db = client.phonebook
 
-# Aggregation
-
+"""
+Calculate the number of people in your phone book who have a birthday this month
+using the aggregation function and map reduce
+"""
+# Aggregation 
+# It should be like that: db.records.aggregate({$project: {month: {$month: "$date_of_birth"}}})
+pipeline = [
+    {"$match": {"$month: $date_of_birth": 4}},
+]
+cursor = db.records.aggregate([]).count()
 # Map-reduce
+
