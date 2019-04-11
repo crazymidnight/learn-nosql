f4678e5c371c5fa814a951b3ab843862c5df4e96
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-04-11 19:39:25 +0700

    add date of birth aggregation via map reduce

--- mongodb/aggregation.py
+++ mongodb/aggregation.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
+from bson import Code
 
 
 client = MongoClient()
@@ -12,15 +13,47 @@ Calculate the number of people in your phone book who have a birthday this month
 using the aggregation function and map reduce
 """
 
-# Aggregation 
+current_month = datetime.now().month
+
+# Aggregation
 pipeline = [
     {"$project": {"month": {"$month": "$date_of_birth"}}},
-    {"$match": {"month": datetime.now().month}},
-    {"$group": {"_id": "$tags", "count": {"$sum": 1}}}
+    {"$match": {"month": current_month}},
+    {"$group": {"_id": "$tags", "count": {"$sum": 1}}},
 ]
 cursor = db.records.aggregate(pipeline)
 for i in cursor:
     number = i
-print(f"Number of people who born at {datetime.now().month}: {number['count']}")
+print(f"Number of people who born at {current_month}: {number['count']}")
 
 # Map-reduce
+
+mapper = Code(
+    """
+    function map() {
+        for(i in this.date_of_birth) {
+		    emit(this.date_of_birth.getMonth(), 1);
+	    }
+    }
+    """
+)
+
+reducer = Code(
+    """
+    function reduce(key, values) {
+        var sum = 0;
+	    for(var i in values) {
+            sum += values[i];
+	    }
+	    return sum;
+    }
+    """
+)
+
+result = int(
+    db.records.map_reduce(mapper, reducer, "myresult").find()[current_month - 1][
+        "value"
+    ]
+)
+
+print(f"Number of people who born at {current_month}: {result}")
